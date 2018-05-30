5850aa7eb473c4a28dd5503d17d2ab87b6339e6d
Brian Haskell <bripod@gmail.com>
2018-05-29 23:08:38 -0400

    fixed card values not showing up for users
    getOwner() for node containing card value needed to be set to user who owns that result.

--- scripts/data_library_talis.lua
+++ scripts/data_library_talis.lua
@@ -18,32 +18,26 @@ function processTalis(sCommand, sParams)
 		else 
 			DB.createNode("talis");
 		end
-		--local sUserName = User;
-		--Debug.chat(sUserName);
-		-- create the Host window 
+		-- create the Host window, not shared with users
 		local wWindowHost = Interface.openWindow("talis_host","talis");
-		-- no need to share, creating the wWindowHost variable creates the window
---		wWindowHost.share();
 		
---		if wWindowClient then			
---			Debug.chat(wWindowClient);
-			local aList = ConnectionManagerADND.getUserLoggedInList();
-			Debug.chat("aList: ",aList);
---			Debug.chat("name in for:",name);
-			-- share this window with every person connected.
-			for _,name in pairs(aList) do
-				local sUserName = "talis." .. name;
---				Debug.chat(sUserName);
-				local wWindowClient = Interface.openWindow("talis_client",sUserName);
-				-- share(): If called as a host, parameters can be used to control the recipient list. 
-				-- this also shares with the host, not sure how to fix, since the host does not show up in the aList
-				-- probably should be called from a "deal" button on the host window
-				-- i.e., host opens /talis, sets up params, hits deal, then players see window
-					-- the sharing code should be moved to the host window anyway, so maybe moot, but will likely need to figure out why this works the way it does anyway
-				wWindowClient.share(name);
-			end
+		local aList = ConnectionManagerADND.getUserLoggedInList();
+		-- share this window with every person connected.
+		for _,name in pairs(aList) do
+
+			local sUserNode = "talis." .. name;
+			local wWindowClient = Interface.openWindow("talis_client",sUserNode);
+
+			DB.setOwner(sUserNode,name);
+			
+			-- share(): If called as a host, parameters can be used to control the recipient list. 
+			-- this also shares with the host, not sure how to fix, since the host does not show up in the aList
+			-- probably should be called from a "deal" button on the host window
+			-- i.e., host opens /talis, sets up params, hits deal, then players see window
+			-- the sharing code should be moved to the host window anyway, so maybe moot, but will likely need to figure out why this works the way it does anyway
+			wWindowClient.share(name);
 		end
---	end
+	end
 end
 
 
