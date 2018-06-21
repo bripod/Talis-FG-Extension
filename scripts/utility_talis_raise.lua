cd7986cf227437fe3646e2dc8ac6deb740baf57a
Brian Haskell <bripod@gmail.com>
2018-06-20 22:58:35 -0400

    MVP achieved
    All the absolutely essential functionality is there. Big to-do is still managing player turn order - currently still need the GM to manage manually & players to behave and not act out of turn

--- scripts/utility_talis_raise.lua
+++ scripts/utility_talis_raise.lua
@@ -1,4 +1,6 @@
 
+
+
 function updatePot(betCP, betSP, betGP, sTalisUserNode)
 	local potCP = DB.getValue("talis.pot.CP") + betCP;
 	local potSP = DB.getValue("talis.pot.SP") + betSP;
@@ -25,28 +27,58 @@ function updateCurrentBet(betCP, betSP, betGP, sTalisUserNode)
 	DB.setValue(sTalisUserNode  .. ".bet." .. sRoundNumber .. ".CP","number",spentCP);
 	DB.setValue(sTalisUserNode  .. ".bet." .. sRoundNumber .. ".SP","number",spentSP);
 	DB.setValue(sTalisUserNode  .. ".bet." .. sRoundNumber .. ".GP","number",spentGP);
-	DB.setValue("talis.currentbetCP","number",spentCP);
-	DB.setValue("talis.currentbetSP","number",spentSP);
-	DB.setValue("talis.currentbetGP","number",spentGP);
+	DB.setValue("talis.pot.currentbetCP","number",spentCP);
+	DB.setValue("talis.pot.currentbetSP","number",spentSP);
+	DB.setValue("talis.pot.currentbetGP","number",spentGP);
+end
+
+function getBetString(betCP,betSP,betGP)
+	local sBet = "";
+	if betGP > 0 then 
+		sBet = sBet .. betGP .. "gp";
+		if betSP > 0 or betCP > 0 then
+			sBet = sBet .. ", ";
+		end
+	end
+	if betSP > 0 then 
+		sBet = sBet .. betSP .. "sp";
+		if betCP > 0 then
+			sBet = sBet .. ", ";
+		end
+	end
+	if betCP > 0 then 
+		sBet = sBet .. betCP .. "cp";
+	end
+	return sBet;
 end
 
 function check(sTalisUserNode,sRoundNumber)
-	--tbd
-	setVisible(false);
-	nextPlayer();
+
+	local rMessage = {};
+	local sChatText = DB.getValue(sTalisUserNode .. ".name") .. " checks";
+	rMessage.text = sChatText;
+	Comm.deliverChatMessage(rMessage);	
+	--setVisible(false);
+	--nextPlayer();
 end
 
 function call(sTalisUserNode,sRoundNumber)
-	-- tbd
+
 	local sRoundNumber = "round" .. DB.getValue("talis.currentround");
-	local spentCP = DB.getValue("talis.currentbetCP") - DB.getValue(sTalisUserNode .. ".bet." .. sRoundNumber .. ".CP");
-	local spentSP = DB.getValue("talis.currentbetSP") - DB.getValue(sTalisUserNode .. ".bet." .. sRoundNumber .. ".SP");
-	local spentGP = DB.getValue("talis.currentbetGP") - DB.getValue(sTalisUserNode .. ".bet." .. sRoundNumber .. ".GP");
+	local spentCP = DB.getValue("talis.pot.currentbetCP") - DB.getValue(sTalisUserNode .. ".bet." .. sRoundNumber .. ".CP");
+	local spentSP = DB.getValue("talis.pot.currentbetSP") - DB.getValue(sTalisUserNode .. ".bet." .. sRoundNumber .. ".SP");
+	local spentGP = DB.getValue("talis.pot.currentbetGP") - DB.getValue(sTalisUserNode .. ".bet." .. sRoundNumber .. ".GP");
 	
 	updateCurrentBet(spentCP, spentSP, spentGP, sTalisUserNode);
 	updatePlayerCommitted(spentCP, spentSP, spentGP, sTalisUserNode);
 	updatePot(spentCP, spentSP, spentGP, sTalisUserNode);
-	nextPlayer();
+	
+	local rMessage = {};
+	local sCallAmount = getBetString(spentCP,spentSP,spentGP);
+	local sChatText = DB.getValue(sTalisUserNode .. ".name") .. " calls for " .. sCallAmount;
+	rMessage.text = sChatText;
+	Comm.deliverChatMessage(rMessage);	
+
 end
 
 function raise(sTalisUserNode,sRoundNumber)
@@ -58,28 +90,31 @@ function raise(sTalisUserNode,sRoundNumber)
 	
 	if DB.getValue(sTalisUserNode .. ".bet." .. sRoundNumber .. ".raise") == 0 then
 	
-		local betCP = DB.getValue("talis.tableRules." .. sRoundNumber .. "bet" .. ".CP");
-		local betSP = DB.getValue("talis.tableRules." .. sRoundNumber .. "bet" .. ".SP"); 
-		local betGP = DB.getValue("talis.tableRules." .. sRoundNumber .. "bet" .. ".GP");
+		local betCP = DB.getValue("talis.tableRules." .. sRoundNumber .. "bet" .. ".CP") + DB.getValue("talis.pot.currentbetCP");
+		local betSP = DB.getValue("talis.tableRules." .. sRoundNumber .. "bet" .. ".SP") + DB.getValue("talis.pot.currentbetSP"); 
+		local betGP = DB.getValue("talis.tableRules." .. sRoundNumber .. "bet" .. ".GP") + DB.getValue("talis.pot.currentbetGP");
 
 		updateCurrentBet(betCP, betSP, betGP, sTalisUserNode);
 		updatePlayerCommitted(betCP, betSP, betGP, sTalisUserNode);
 		updatePot(betCP, betSP, betGP, sTalisUserNode);
-		nextPlayer();
-	
 		DB.setValue(sTalisUserNode .. ".bet." .. sRoundNumber .. ".raise","number",1);
-
-		local sChatText = DB.getValue(sTalisUserNode .. ".name") .. " raises"
+		local sRaiseAmount = getBetString(betCP,betSP,betGP);
+		local sChatText = DB.getValue(sTalisUserNode .. ".name") .. " raises to " .. sRaiseAmount;
 		rMessage.text = sChatText;
 	else
-		local sChatText = DB.getValue(sTalisUserNode .. ".name") .. " has already raised this round"
+		local sChatText = DB.getValue(sTalisUserNode .. ".name") .. " has already raised this round!"
 		rMessage.text = sChatText;
 	end
 	Comm.deliverChatMessage(rMessage);
+
 end
 
 function fold(sTalisUserNode,sRoundNumber)
-	-- tbd
+	local rMessage = {};
+	local sChatText = DB.getValue(sTalisUserNode .. ".name") .. " folds!";
+	rMessage.text = sChatText;
+	rMessage.mode = "emote";
+	Comm.deliverChatMessage(rMessage);
 	if getValue() == 0 then
 		if window.delete then
 			window.delete();
@@ -89,14 +124,9 @@ function fold(sTalisUserNode,sRoundNumber)
 	end
 end
 
-function nextPlayer()
-	-- tbd
-end
-
 function onButtonPress()
 	local sTalisUserNode = DB.getPath(window.getDatabaseNode());
 	local sRoundNumber = "round" .. DB.getValue("talis.currentround");
-	
 	if getName() == "checkbutton" then
 		check(sTalisUserNode,sRoundNumber);
 	elseif getName() == "callbutton" then
